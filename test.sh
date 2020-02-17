2a653c7a79a33152661afc37d7bb2958db660655
sekhar <sekhar>
2020-02-17 15:46:06 +0530

    TEST COMMIT

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ 
+### Print Arguments and their values
+
+echo "Total Arguments:" $#
+echo "All Arguments values:" $@
+
+### Command arguments can be accessed as
+
+echo "First->" $1
+echo "Second->" $2
+echo "Third->" $3
